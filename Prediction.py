cd2569743be246a5b6ae4799e00db247e4e133b9
coffincw <coffincw@gmail.com>
2017-10-21 15:59:26 -0400

    created prediction class

---
+++ Prediction.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+month = "Month"
+coal = "Coal, Including Coal Coke Net Imports"
+natural_gas = "Natural Gas, Excluding Supplemental Gaseous Fuels"
+aviation = "Aviation Gasoline"
+distillate = "Distillate Fuel Oil, Excluding Biodiesel"
+hydrocarbon = "Hydrocarbon Gas Liquids"
+jet_fuel = "Jet Fuel"
+kerosene = "Kerosene"
+lubricants = "Lubricants"
+motor_gasoline = "Motor Gasoline, Excluding Ethanol"
+
+def predictionData() :
+    i = 5
